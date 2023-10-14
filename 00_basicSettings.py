52ce5f7a053596d42f767e5e5f9e768756c2dbe3
Roberto Fantini <roberto.fantini@gmail.com>
2023-10-14 17:20:38 +0200

    Exercises from Chapter 2

---
+++ 00_basicSettings.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels as sm
+
+pd.options.display.max_columns  = 20
+pd.options.display.max_rows = 20
+pd.options.display.max_colwidth = 80
+np.set_printoptions(precision=4, suppress=True)
+
+
